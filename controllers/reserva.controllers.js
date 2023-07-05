ec695afb018d06ae9472550a318b3b432dcabed3
maximilianopietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-07-05 15:30:54 -0300

    he creado los controladores para el CRUD

--- controllers/reserva.controllers.js
+++ controllers/reserva.controllers.js
@@ -1,13 +1,74 @@
-const ctrlReservas = {};
+const ctrlReservas = require("../models/Reserva")
 
-// ==========================================
-//         Rutas para CRUD de reservas
-// ==========================================
 
-// Obtener todas las reservas
-// Obtener una reserva
-// Crear una reserva
-// Actualizar una reserva
-// Eliminar una reserva de forma lógica
+const listarReservas = async (req, res) => {
+    try {
+        const reservas = await ctrlReservas.findAll();
+        res.render("index", { reservas : reservas });
+    } catch (error) {
+        res.status(500).json({ error : "ocurrio un error en el servidor" });
+        console.log(error)
+    }
+};
 
-module.exports = ctrlReservas;
+const mostrarFormularioCrear = (req, res) => {
+    res.render("create");
+  };
+
+const guardarReserva = async (req, res) => {
+    try {
+        const reservas = await ctrlReservas.create(reservas);
+        res.redirect("/api/reservas");
+    } catch (error) {
+        res.status(500).json({ error : "ocurrio un error en el servidor" });
+        console.log(error)
+    }
+};
+
+const mostrarFormularioEditar = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const reservas = await ctrlReservas.findByPk(id);
+        res.render("editar", { reservas});
+    } catch (error) {
+        res.status(500).json({ error : "ocurrio un error en el servidor" });
+        console.log(error)
+    }
+};
+
+const editarReserva = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const reservas = req.body;
+        await ctrlReservas.update(reservas, { where: { idReserva: id } });
+        res.redirect("/api/reservas");
+    } catch (error) {
+        res.status(500).json({ error : "ocurrio un error en el servidor" });
+        console.log(error)
+    }
+};
+
+const eliminarReserva = async (req, res) => {
+    try {
+      const id = req.params.id;
+      await Reservas.destroy({ where: { id } });
+      res.redirect("/api/reservas");
+    } catch (error) {
+      res.status(500).json({
+        error: "Ocurrió un error en el servidor",
+      });
+      console.log(error);
+    }
+  };
+
+
+
+
+module.exports = {
+    listarReservas,
+    mostrarFormularioCrear,
+    guardarReserva,
+    mostrarFormularioEditar,
+    editarReserva,
+    eliminarReserva
+};
