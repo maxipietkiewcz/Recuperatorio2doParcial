81ba34ccf5cbcdfc3890d5f60f73739b987c34b2
maximilianopietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-07-05 14:43:26 -0300

    he echo cambios en app.js. Requiriendo los middleware, el body-parser y configurandolo, tambien he inicializado el motor de plantilla. Por ultimo he levantado el server

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-// Imports
-const cors = require('cors');
-const express = require('express');
-
-const path = require('path');
-
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
-// Middlewares
-// TODO: Implementar middlewares
+//requiriendo middleware
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 
-app.use(express.static(path.join(__dirname, 'public')));
+//requiriendo body parser
+const bodyParser = require("body-parser");
+//Configuracion de bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
-app.use('/api', require('./routes/reserva.routes'));
+//motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("public"));
+
+
+
+
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
-// Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+//levantar el servidor
+const puerto = process.env.PORT || 3000;
+app.listen(puerto, (req, res) => {
+  try {
+    console.log(
+      "El server esta conectado en http://localhost:" + puerto + "/api/reservas"
+    );
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
